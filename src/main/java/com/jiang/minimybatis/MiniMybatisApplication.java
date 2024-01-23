e0221ab6e5c509de1f3dff3ea80dae26f90af9fa
jiangyunkai <jiangyunkai@kuaishou.com>
2024-01-23 16:16:19 +0800

    init

---
+++ src/main/java/com/jiang/minimybatis/MiniMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.jiang.minimybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiang/minimybatis/MiniMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.minimybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
